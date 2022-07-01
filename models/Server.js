0048816f317e05ce4b3281b1fde2e7763ded3620
Brian Alvarez <brianalvarez2000@gmail.com>
2022-07-01 07:30:29 -0500

    Add connection Mongo Compass and create model Usuarios

--- models/Server.js
+++ models/Server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const { dbConnection } = require('../database/configDb')
 
 class Server {
   constructor () {
     this.app = express()
     this.port = process.env.PORT
     this.usuariosPath = '/api/usuarios'
+    this.conectarDB()
     this.middlewares()
     this.routes()
+  }
+
+  async conectarDB () {
+    await dbConnection()
   }
 
   middlewares () {

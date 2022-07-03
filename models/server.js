0b6fed15d6ae7cc8f8a5107887e8fde90509b1b3
Brian Alvarez <brianalvarez2000@gmail.com>
2022-07-03 15:16:21 -0500

    Busquedas flexibles - Cualquier coleccion, Mongo ID o nombres

--- models/server.js
+++ models/server.js
@@ -8,6 +8,7 @@ class Server {
     this.port = process.env.PORT
     this.paths = {
       auth: '/api/auth',
+      buscar: '/api/buscar',
       categorias: '/api/categorias',
       productos: '/api/productos',
       usuarios: '/api/usuarios'
@@ -29,6 +30,7 @@ class Server {
 
   routes () {
     this.app.use(this.paths.auth, require('../routes/auth.routes'))
+    this.app.use(this.paths.buscar, require('../routes/buscar.routes'))
     this.app.use(this.paths.categorias, require('../routes/categorias.routes'))
     this.app.use(this.paths.productos, require('../routes/productos.routes'))
     this.app.use(this.paths.usuarios, require('../routes/usuarios.routes'))

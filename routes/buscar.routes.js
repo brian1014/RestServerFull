18d9bf96ff3a03459be270514355e4433a2469ac
Brian Alvarez <brianalvarez2000@gmail.com>
2022-08-02 09:58:21 -0500

    Update

--- routes/buscar.routes.js
+++ routes/buscar.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { buscar } = require('../controllers/buscar.routes')
+const { buscar } = require('../controllers/buscar.controller')
 const router = Router()
 
 router.get('/:coleccion/:termino', buscar)
